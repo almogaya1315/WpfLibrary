889fa77aa083f1f3877782f6284e78321da62aa2
almogaya1315 <liormatsliah1985@gmail.com>
2017-08-17 20:32:36 +0300

    flow

--- DataGridEx/Main/Person.cs
+++ DataGridEx/Main/Person.cs
@@ -8,6 +8,17 @@ namespace Main
 {
     public class Person
     {
+        public bool IsAdult
+        {
+            get
+            {
+                if (Age >= 18)
+                    return true;
+                else return false;
+            }
+            set { }
+        }
+
         public int Id { get; set; }
 
         public string Name { get; set; }
@@ -15,5 +26,20 @@ namespace Main
         public double Age { get; set; }
 
         public int Year { get; set; }
+
+        IList<Person> _relatives;
+        public IList<Person> Relatives
+        {
+            get
+            {
+                if (_relatives == null)
+                    _relatives = new List<Person>();
+                return _relatives;
+            }
+            set
+            {
+                _relatives = value;
+            }
+        }
     }
 }
--- DataGridEx/Main/ViewModel/MainViewModel.cs
+++ DataGridEx/Main/ViewModel/MainViewModel.cs
@@ -28,10 +28,31 @@ namespace Main.ViewModel
         {
             Control = new DataControl();
 
-            People.Add(new Person() { Id = 1, Name = "lior", Age = 32, Year = 1985 });
-            People.Add(new Person() { Id = 2, Name = "keren", Age = 33, Year = 1984 });
-            People.Add(new Person() { Id = 3, Name = "gaya", Age = 4, Year = 2013 });
-            People.Add(new Person() { Id = 4, Name = "almog", Age = 2, Year = 2015 });
+            var lior = new Person() { Id = 1, Name = "lior", Age = 32, Year = 1985 };
+            var keren = new Person() { Id = 2, Name = "keren", Age = 33, Year = 1984 };
+            var gaya = new Person() { Id = 3, Name = "gaya", Age = 4, Year = 2013 };
+            var almog = new Person() { Id = 4, Name = "almog", Age = 2, Year = 2015 };
+
+            lior.Relatives.Add(keren);
+            lior.Relatives.Add(gaya);
+            lior.Relatives.Add(almog);
+
+            keren.Relatives.Add(lior);
+            keren.Relatives.Add(gaya);
+            keren.Relatives.Add(almog);
+
+            gaya.Relatives.Add(lior);
+            gaya.Relatives.Add(keren);
+            gaya.Relatives.Add(almog);
+
+            almog.Relatives.Add(lior);
+            almog.Relatives.Add(keren);
+            almog.Relatives.Add(gaya);
+
+            People.Add(lior);
+            People.Add(keren);
+            People.Add(gaya);
+            People.Add(almog);
         }
     }
 }
