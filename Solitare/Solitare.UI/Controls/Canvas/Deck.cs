f7215b890ada00f4cf698a8ecb6bdefa5a0836b8
almogaya1315 <liormatsliah1985@gmail.com>
2017-10-10 00:24:52 +0300

    flow

---
+++ Solitare/Solitare.UI/Controls/Canvas/Deck.cs
@@ -0,0 +1,47 @@
+﻿using Solitare.UI.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Solitare.UI.Controls.Canvas
+{
+    public class Deck : System.Windows.Controls.Canvas
+    {
+        public static readonly DependencyProperty DeckNameProperty =
+            DependencyProperty.Register("DeckName", typeof(DeckName), typeof(Deck), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty IsDraggableProperty =
+            DependencyProperty.Register("IsDraggable", 
+                typeof(bool), typeof(Deck), 
+                new PropertyMetadata(false, OnIsDraggablePropertyChanged));
+
+        public DeckName DeckName
+        {
+            get { return (DeckName)GetValue(DeckNameProperty); }
+            set { SetValue(DeckNameProperty, value); }
+        }
+
+        public bool IsDraggable
+        {
+            get { return (bool)GetValue(IsDraggableProperty); }
+            set { SetValue(IsDraggableProperty, value); }
+        }
+
+        private static void OnIsDraggablePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var deck = (Deck)d;
+            if (deck == null) return;
+
+            var isDraggable = (bool)d.GetValue(IsDraggableProperty);
+
+
+            if (isDraggable)
+            {
+
+            }
+        }
+    }
+}
--- Solitare/Solitare.UI/Game/Views/GameView.xaml.cs
+++ Solitare/Solitare.UI/Game/Views/GameView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Solitare.UI.Controls.Image;
+﻿using Solitare.UI.Controls.Canvas;
+using Solitare.UI.Controls.Image;
 using Solitare.UI.Enums;
 using Solitare.UI.Game.ViewModels;
 using System;
@@ -18,7 +19,7 @@ namespace Solitare.UI.Game.Views
         private DropData _currentDrag;
         private bool _isDrag;
 
-        private List<Canvas> _closedDecks;
+        private List<Deck> _closedDecks;
         private Canvas _mainCanvas;
         private Card _moveableCard;
 
@@ -29,38 +30,23 @@ namespace Solitare.UI.Game.Views
             _gameViewModel = (GameViewModel)DataContext;
 
             _mainCanvas = MainCanvas;
-            _closedDecks = new List<Canvas>() { OpenDeckCard, DiamondsDeckCard, ClubsDeckCard, HeartsDeckCard, SpadesDeckCard };
+            _closedDecks = new List<Deck>() { OpenDeckCard, DiamondsDeckCard, ClubsDeckCard, HeartsDeckCard, SpadesDeckCard };
 
             AddHandler(MouseMoveEvent, new MouseEventHandler(OnMouseMoveChanged));
         }
 
         private void MoveableCard_MouseLeftButtonDown(object sender, MouseButtonEventArgs args)
         {
-            if (_moveableCard.isOverOpenDeck)
-            {
-                DropCard(OpenDeckCard, DeckName.OpenDeckCard);
-            }
-            else if (_moveableCard.IsOverDiamondsDeck)
-            {
-                DropCard(DiamondsDeckCard, DeckName.DiamondsDeckCard);
-            }
-            else if (_moveableCard.IsOverClubsDeck)
-            {
-                DropCard(ClubsDeckCard, DeckName.ClubsDeckCard);
-            }
-            else if (_moveableCard.IsOverHeartsDeck)
-            {
-                DropCard(HeartsDeckCard, DeckName.HeartsDeckCard);
-            }
-            else if (_moveableCard.IsOverSpadesDeck)
-            {
-                DropCard(SpadesDeckCard, DeckName.SpadesDeckCard);
-            }
+            if (_moveableCard.isOverOpenDeck) DropCard(OpenDeckCard);
+            else if (_moveableCard.IsOverDiamondsDeck) DropCard(DiamondsDeckCard);
+            else if (_moveableCard.IsOverClubsDeck) DropCard(ClubsDeckCard);
+            else if (_moveableCard.IsOverHeartsDeck) DropCard(HeartsDeckCard);
+            else if (_moveableCard.IsOverSpadesDeck) DropCard(SpadesDeckCard);
         }
 
-        private void DropCard(Canvas deck, DeckName deckName)
+        private void DropCard(Deck deck)
         {
-            _gameViewModel.DropCard(deckName);
+            _gameViewModel.DropCard(deck.DeckName);
 
             deck.Background = null;
             _mainCanvas.Children.Remove(_moveableCard);
@@ -78,7 +64,7 @@ namespace Solitare.UI.Game.Views
             }
         }
 
-        private void FindOverDeck(Canvas deck, MouseEventArgs args)
+        private void FindOverDeck(Deck deck, MouseEventArgs args)
         {
             var point = deck.TransformToAncestor(Application.Current.MainWindow)
                                                             .Transform(new Point(0, 0));
@@ -96,27 +82,27 @@ namespace Solitare.UI.Game.Views
             }
         }
 
-        private void SetIsMouseOver(Canvas deck, bool isOver)
+        private void SetIsMouseOver(Deck deck, bool isOver)
         {
-            switch (deck.Name)
+            switch (deck.DeckName)
             {
-                case "OpenDeckCard":
+                case DeckName.OpenDeckCard:
                     deck.Background = isOver ? Brushes.Red : null;
                     _moveableCard.isOverOpenDeck = isOver;
                     break;
-                case "DiamondsDeckCard":
+                case DeckName.DiamondsDeckCard:
                     deck.Background = isOver ? Brushes.Black : null;
                     _moveableCard.IsOverDiamondsDeck = isOver;
                     break;
-                case "SpadesDeckCard":
+                case DeckName.SpadesDeckCard:
                     deck.Background = isOver ? Brushes.Red : null;
                     _moveableCard.IsOverSpadesDeck = isOver;
                     break;
-                case "HeartsDeckCard":
+                case DeckName.HeartsDeckCard:
                     deck.Background = isOver ? Brushes.Black : null;
                     _moveableCard.IsOverHeartsDeck = isOver;
                     break;
-                case "ClubsDeckCard":
+                case DeckName.ClubsDeckCard:
                     deck.Background = isOver ? Brushes.Red : null;
                     _moveableCard.IsOverClubsDeck = isOver;
                     break;
